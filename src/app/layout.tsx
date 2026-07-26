2467147cdf292e88329e9c350e24346fcc9601b4
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2026-07-26 00:29:30 -0300

    setup base ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <div className="root flex min-h-full flex-col flex-1">{children}</div>
+      </body>
     </html>
   );
 }
